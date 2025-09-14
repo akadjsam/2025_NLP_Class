35d1cddb332382bab461e43523fe586eb18c72f4
akadjsam <akadjsam@gmail.com>
2025-09-14 19:31:42 +0900

    pandas sampling

--- week02_pandas_basic.py
+++ week02_pandas_basic.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import seaborn as sns
 
 df = pd.DataFrame(
     {"id" : [100, 102, 107],
@@ -38,8 +38,29 @@ df1 = pd.DataFrame(
     },
     index=[1, 2, 3, 4]
 )
-print(df1)
+# print(df1)
 
 df2 = df1.pivot(index='date', columns='city', values='temp')
-print()
-print(df2)
+# print()
+# print(df2)
+
+df = sns.load_dataset('mpg') # mpg는 seaborn 패키지에서 제공하는 데이터셋
+# print(df.tail()) # tail은 아래서부터 5개, head는 위에서부터 5개
+# print(df) # 397행, 9개 열
+
+# print(df.sort_values('mpg')) # mpg(연비) 기준으로 오름차순
+# print(df.sort_values('mpg', ascending=False)) # mpg기준으로 내림차순
+# print(df.drop(columns=['mpg', 'cylinders'])) # 보기 싫은 컬럼을 삭제하기
+
+
+print(sns.get_dataset_names()) # seaborn 에서 제공하는 데이터셋들의 이름을 출력
+titanic = sns.load_dataset('titanic')
+# print(titanic)
+# print(titanic[['sibsp', 'parch', 'fare']])
+# print(titanic.columns)
+
+print(titanic[titanic.age > 70]) # 70세 이상의 행만 남김
+print(titanic.sample(n=7)) # 랜덤하게 7개 추출
+print(titanic.nsmallest(4, 'age')) # 가장 나이가 어린 4명
+print(titanic.nlargest(4, 'age')) # 가장 나이가 많은 4명
+
